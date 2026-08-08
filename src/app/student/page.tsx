f26905999543fe47f454b9301896aa9262c234ae
rootanvir <tanvirahmed131015@gmail.com>
2026-08-09 00:03:27 +0600

    student card

---
+++ src/app/student/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import Image from "next/image";
+import StudentCard from "./ui/studentCard";
+
+export default function Student() {
+  return (
+    <main>
+        <StudentCard/>
+    </main>
+  );
+}
+
